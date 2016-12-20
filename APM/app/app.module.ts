1c88f568c25b39964aeda84d1c5898a94d90162c
Allen Grant <agrant@invsqr.com>
2016-12-20 09:22:27 -0500

    Finishing up the Navigation and Routing Basics module.

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -13,14 +14,21 @@ import { WelcomeComponent } from './home/welcome.component';
 @NgModule({
   imports: [BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   declarations: [AppComponent,
     ProductListComponent
-    ,ProductFilterPipe
-    ,StarComponent
-    ,ProductDetailComponent
-    ,WelcomeComponent
+    , ProductFilterPipe
+    , StarComponent
+    , ProductDetailComponent
+    , WelcomeComponent
   ],
   bootstrap: [AppComponent]
 })
