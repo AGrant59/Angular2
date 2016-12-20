42ce499d8a3fe3d61d03d40b07cbe0f5d4236a79
Allen Grant <agrant@invsqr.com>
2016-12-20 13:39:16 -0500

    Finished the FeatureModules section.

---
+++ APM/app/products/product.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductListComponent } from './product-list.component';
+import { ProductFilterPipe } from './product-filter.pipe';
+import { StarComponent } from '../shared/star.component';
+import { ProductDetailGuard } from './product-guard.service';
+import { ProductService } from './product.service';
+
+@NgModule({
+    declarations: [
+        ProductDetailComponent,
+        ProductListComponent,
+        ProductFilterPipe,
+        StarComponent
+    ],
+    imports: [
+        FormsModule,
+        CommonModule,
+        RouterModule.forChild([
+            { path: 'products', component: ProductListComponent },
+            { path: 'product/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent }
+        ])
+    ],
+    providers: [
+        ProductDetailGuard,
+        ProductService
+    ]
+})
+export class ProductModule { }
